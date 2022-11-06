a44c3f55088e5fb64afd6485fd250afd3c4d9f53
nishkarta <nishkarta@gmail.com>
2022-11-07 04:29:39 +0700

    test

--- src/components/MapEl.js
+++ src/components/MapEl.js
@@ -35,35 +35,35 @@ const AddMarker = () => {
     return null;
 };
 
-const Geocoder = ({ form, setForm, userDefaultLocation }) => {
-    const map = useMap();
-    // show marker user location when user open map
-    userDefaultLocation &&
-        L.marker({
-            lat: userDefaultLocation.split(',')[0],
-            lng: userDefaultLocation.split(',')[1],
-        })
-            .addTo(map)
-            .bindPopup('hahaha')
-            .openPopup();
-    // event click on map
-    map.on('click', (e) => {
-        setForm({ ...form, location: `${e.latlng.lat},${e.latlng.lng}` });
-        L.marker(e.latlng).addTo(map).bindPopup().openPopup();
-    });
-    // search map control
-    L.Control.geocoder({
-        defaultMarkGeocode: false,
-    })
-        .on('markgeocode', function (e) {
-            var latlng = e.geocode.center;
-            setForm({ ...form, location: `${latlng.lat},${latlng.lng}` });
-            L.marker(latlng).addTo(map).bindPopup(e.geocode.name).openPopup();
-            map.fitBounds(e.geocode.bbox);
-        })
-        .addTo(map);
-    return null;
-};
+// const Geocoder = ({ form, setForm, userDefaultLocation }) => {
+//     const map = useMap();
+//     // show marker user location when user open map
+//     userDefaultLocation &&
+//         L.marker({
+//             lat: userDefaultLocation.split(',')[0],
+//             lng: userDefaultLocation.split(',')[1],
+//         })
+//             .addTo(map)
+//             .bindPopup('hahaha')
+//             .openPopup();
+//     // event click on map
+//     map.on('click', (e) => {
+//         setForm({ ...form, location: `${e.latlng.lat},${e.latlng.lng}` });
+//         L.marker(e.latlng).addTo(map).bindPopup().openPopup();
+//     });
+//     // search map control
+//     L.Control.geocoder({
+//         defaultMarkGeocode: false,
+//     })
+//         .on('markgeocode', function (e) {
+//             var latlng = e.geocode.center;
+//             setForm({ ...form, location: `${latlng.lat},${latlng.lng}` });
+//             L.marker(latlng).addTo(map).bindPopup(e.geocode.name).openPopup();
+//             map.fitBounds(e.geocode.bbox);
+//         })
+//         .addTo(map);
+//     return null;
+// };
 const GeoRouting = ({ partnerLocation }) => {
     const map = useMap();
     // const { userProfile, refetch } = useContext(UserContext);
